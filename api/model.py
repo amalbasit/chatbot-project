04c1c9cb251aeef18fffad3a252b00c8d693cc19
Amal Basit <amalbasit2004@gmail.com>
2025-08-15 16:05:17 +0500

    First commit

--- api/model.py
+++ api/model.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 
 class Request(BaseModel):
+    uuid: str
     message: str
+    type: str
 
 class Response(BaseModel):
     reply: str
--- main.py
+++ main.py
@@ -1,60 +1,58 @@
 import streamlit as st
 import requests
+import uuid
 
-API_URL = "http://127.0.0.1:8000/chat"  # your FastAPI URL
 
-# --- Initialize session state ---
+API_URL = "http://127.0.0.1:8000/chat"
+
+# Session states
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 if "last_message" not in st.session_state:
     st.session_state.last_message = ""
 
-
-# bot reply
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
 
 def get_bot_response(prompt):
     try:
-        response = requests.post(
-            API_URL, 
-            json={"message": prompt}       
-        )
-        # Extract reply from JSON
+        msg_info = {
+            "uuid": st.session_state.session_id,
+            "message": prompt,
+            "type": "text"
+        }
+        response = requests.post(API_URL, json=msg_info)
         return response.json().get("reply", "No reply received")
     except Exception as e:
-        print(f"Error: {e}") # DEBUGGING, ACC WRITE reply_text = "⚠️ Could not reach server. Please try again."
+        print(f"Error: {e}")
 
-# --- Function to send message ---
 def send_message():
-    prompt = st.session_state.user_input.strip()
+    prompt = st.session_state.user_input
     reply_text = ""
 
-    # Avoid duplicates or empty messages
     if prompt and prompt != st.session_state.last_message:
         st.session_state.messages.append({"role": "user", "content": prompt})
         st.session_state.last_message = prompt
 
         reply_text = get_bot_response(prompt)
 
-        st.session_state.user_input = ""  # safely clears input
+        st.session_state.user_input = ""
 
-    # Append AI reply to chat
     st.session_state.messages.append({"role": "ai", "content": reply_text})
 
-message_container = st.container(height=680, border=False) # acts as a div
+message_container = st.container(height=680, border=False)
 
 with message_container:
     for message in st.session_state.messages:
         with st.chat_message(message['role']):
             st.write(message['content'])
 
-# --- Input row layout ---
 col_plus, col_input, col_send = st.columns([0.7, 8, 1.2], vertical_alignment='bottom', gap='small')
 
 with col_plus:
     st.button(label="", icon="➕")
 
-# Text input with on_change callback
 with col_input:
     st.text_input(
         label="Message",
@@ -64,7 +62,6 @@ with col_input:
         on_change=send_message
     )
 
-# Send button triggers the same function
 with col_send:
     if st.button("Send", type="primary"):
         send_message()
