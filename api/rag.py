8d10f283b3c6f313407fbe8474d365309574caf3
Amal Basit <amalbasit2004@gmail.com>
2025-08-20 17:31:32 +0500

    RAG pipeline implementation

--- api/rag.py
+++ api/rag.py
@@ -1,42 +1,44 @@
 import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+import shutil
 
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.document_loaders import TextLoader
+from api.data_loader import split_docs
+from api.vector_store import init_vector_store
+from api.prompt import build_prompt
+from api.llm_query import llm_query_with_retriever
+from langchain.schema import Document
 
-from constants import DATA_PATH
+class RAGPipeline:
+    def __init__(self, vector_name, template_string):
+        self.vector_store = init_vector_store(vector_name)  
+        self.prompt_template = build_prompt(template_string)
 
-# Load Docs
-def load_documents():
-    docs = []
-    for file in os.listdir(DATA_PATH):
-        if file.endswith(".txt"):
-            loader = TextLoader(os.path.join(DATA_PATH, file))
-            docs.extend(loader.load())
-    return docs
+    def add_document(self, content: str, session_id: str):
+        # wrap content in Document
+        doc = Document(page_content=content)
+        chunks = split_docs(chunk_size=800, chunk_overlap=150, docs=[doc])
+        for chunk in chunks:
+            chunk.metadata = {"session_id": session_id}
+        print(f"Split into {len(chunks)} chunks for session {session_id}")
+        self.vector_store.add_documents(chunks)
+        # print(self.vector_store)
 
-docs = load_documents()
+    def query(self, question: str, session_id: str):
+        docs = self.vector_store.similarity_search(
+            query=question,
+            k=5,  # number of results to return
+            filter={"session_id": session_id}
+        )
+        return docs
 
-# Split into chunks
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=500,
-    length_function=len,
-    add_start_index=True
-)
+    def query_llm(self, question: str, session_id: str):
+        # Create a retriever filtered by session_id
+        retriever = self.vector_store.as_retriever(
+            search_kwargs={"filter": {"session_id": session_id}}
+        )
 
-chunks = text_splitter.split_documents(docs)
+        # Call LLM with the filtered retriever and the PromptTemplate object directly
+        return llm_query_with_retriever(self.vector_store, self.prompt_template, question, retriever)
 
 
-# Embeddings
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
-# Vector Store
-vector_store = Chroma(
-    collection_name="example_collection",
-    embedding_function=embeddings,
-    persist_directory="./chroma_langchain_db",
-)
-
-vector_store.add_documents(chunks)
