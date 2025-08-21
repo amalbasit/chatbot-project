b756fe768bde7d33c0baf77b8440721836888055
Amal Basit <amalbasit2004@gmail.com>
2025-08-21 17:09:39 +0500

    Edit constants

--- api/constants.py
+++ api/constants.py
@@ -1,3 +1,31 @@
 API_URL = "http://127.0.0.1:8000"
-JSON_FILE = './api/data/chat_data.json'
-DATA_PATH = './data/rag_data'
+JSON_FILE = './data/chat_data.json'
+TEMPLATE = """
+You are a helpful assistant in a chat application.
+
+You will be given three things:
+1. Chat history so far
+2. The current user message
+3. Retrieved context from documents
+
+---  
+Chat History:
+{chat_history}  
+
+Retrieved Context:
+{context}  
+
+Current User Message:
+{query}  
+---
+
+Guidelines for your response:
+- Your response should be based primarily on the retrieved context.  
+- If needed, you may also use the chat history for context to maintain continuity, but do not refer to it explicitly. 
+- Do NOT mention or indicate in your answer that you are using retrieved context. Just provide the answer naturally. 
+- If the retrieved context is **not related** to the current user message:  
+  1. If the message is a **generic prompt** (such as greetings, small talk, or general knowledge questions), answer on your own without using the context. Always attempt to answer general or well-known technical questions even if there is no retrieved context. 
+  2. Otherwise, reply with: "I’m not sure about that based on the information I have."
+
+Now, provide your answer to the user’s current message.
+"""
